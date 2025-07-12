badd67b38e32bdbdd9f17cedcd18d080ede551d0
JohnJohnsonTaren <61081845+JohnJohnsonTaren@users.noreply.github.com>
2025-07-12 12:23:03 +0300

    Add files via upload

--- src/main/java/DatabaseQueryService.java
+++ src/main/java/DatabaseQueryService.java
@@ -7,66 +7,155 @@
 // Кожний метод називай згідно Java Code Conventions.
 // Зверни увагу на коректний тип значення, що повертатиме метод.
 // Наприклад, для файлу find_max_projects_client сигнатура методу
-//      виглядатиме List<MaxProjectCountClient> findMaxProjectsClient().
-// При цьому клас MaxProjectCountClient необхідно описати, наприклад:
-//      public class MaxProjectCountClient {
+//      виглядатиме List<SQLResources.MaxProjectCountClient> findMaxProjectsClient().
+// При цьому клас SQLResources.MaxProjectCountClient необхідно описати, наприклад:
+//      public class SQLResources.MaxProjectCountClient {
 //          private String name;
 //          private int projectCount;
 //      }
 //
-// Для роботи з БД використовуй написаний раніше тобою клас Database.
+// Для роботи з БД використовуй написаний раніше тобою клас BehaviorDB.Database.
 // Результат виконання завдання - методи для кожного SELECT запиту,
 //      які можна викликати, наприклад, наступним чином:
-//      List<MaxProjectCountClient> maxProjectCountClients =
+//      List<SQLResources.MaxProjectCountClient> maxProjectCountClients =
 //          new DatabaseQueryService().findMaxProjectsClient();
 // Запусти так кожний метод, і переконайсь, що він повертає коректну інформацію
 //      і під час запуску ніде не виникають Exceptions.
 
+import BehaviorDB.Database;
+import BehaviorDB.DatabaseInitService;
+import BehaviorDB.DatabasePopulateService;
+import SQL_QueryResources.LongestProject;
+import SQL_QueryResources.MaxProjectCountClient;
+import SQL_QueryResources.MaxSalaryWorker;
+import SQL_QueryResources.YoungestEldestWorkers;
+
+import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.sql.Connection;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
 
 public class DatabaseQueryService {
-    public List<MaxProjectCountClient> findMaxProjectsClient() throws Exception {
-        List<MaxProjectCountClient> result = new ArrayList<>();
+    public List<LongestProject> findLongestProject() throws Exception {
+        List<LongestProject> result = new ArrayList<>();
+        String sqlQuery = "";
+        try {
+            sqlQuery = Files.readString(Paths.get("sql/find_longest_project.sql"));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
         Connection connection = Database.getInstance().getConnection();
         Statement statement = connection.createStatement();
+        try (ResultSet resultSet = statement.executeQuery(sqlQuery)) {
+            while (resultSet.next()) {
+                String nameClient = resultSet.getString("name");
+                result.add(new LongestProject(nameClient));
+            }
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+        }
+        return result;
+    }
 
-        String sqlQuery = Files.readString(Paths.get("sql/find_max_projects_client.sql"));
-        ResultSet resultSet = null;
-
+    public List<MaxProjectCountClient> findMaxProjectsClient() throws Exception {
+        List<MaxProjectCountClient> result = new ArrayList<>();
+        String sqlQuery = "";
         try {
-            resultSet = statement.executeQuery(sqlQuery);
+            sqlQuery = Files.readString(Paths.get("sql/find_max_projects_client.sql"));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        Connection connection = Database.getInstance().getConnection();
+        Statement statement = connection.createStatement();
+        try (ResultSet resultSet = statement.executeQuery(sqlQuery)) {
             while (resultSet.next()) {
                 String clientName = resultSet.getString("name");
                 int projectCount = resultSet.getInt("project_count");
                 result.add(new MaxProjectCountClient(clientName, projectCount));
+            }
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+        }
+        return result;
+    }
 
+    public List<MaxSalaryWorker> findMaxSalaryWorker() throws Exception {
+        List<MaxSalaryWorker> result = new ArrayList<>();
+        String sqlQuery = "";
+        try {
+            sqlQuery = Files.readString(Paths.get("sql/find_max_salary_worker.sql"));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
 
+        Connection connection = Database.getInstance().getConnection();
+        Statement statement = connection.createStatement();
+        try (ResultSet resultSet = statement.executeQuery(sqlQuery)) {
+            while (resultSet.next()) {
+                String clientName = resultSet.getString("name");
+                int salary = resultSet.getInt("salary");
+                result.add(new MaxSalaryWorker(clientName, salary));
+            }
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+        }
+        return result;
+    }
+
+    public List<YoungestEldestWorkers> findYoungestEldestWorkers() throws Exception {
+        List<YoungestEldestWorkers> result = new ArrayList<>();
+        String sqlQuery = "";
+        try {
+            sqlQuery = Files.readString(Paths.get("sql/find_youngest_eldest_workers.sql"));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        Connection connection = Database.getInstance().getConnection();
+        Statement statement = connection.createStatement();
+        try (ResultSet resultSet = statement.executeQuery(sqlQuery)){
+            while (resultSet.next()) {
+                String clientName = resultSet.getString("name");
+                Date birthday = resultSet.getDate("birthday");
+                result.add(new YoungestEldestWorkers(birthday, clientName));
             }
         } catch (SQLException ex) {
             ex.printStackTrace();
         }
 
-        resultSet.close();
-        statement.close();
         return result;
     }
 
     public static void main(String[] args) throws Exception {
         DatabaseInitService.main(args); // Ініціалізація структури БД
-
         DatabasePopulateService.main(args); // Наповнення БД даними
-
         DatabaseQueryService queryService = new DatabaseQueryService(); // Запуск DatabaseQueryService
-        List<MaxProjectCountClient> maxProjectClients = queryService.findMaxProjectsClient();
 
+        //+
+        List<LongestProject> longestProject = queryService.findLongestProject();
+        for (LongestProject client: longestProject) {
+            System.out.println(client);
+        }
+
+        //-
+        List<MaxProjectCountClient> maxProjectClients = queryService.findMaxProjectsClient();
         for (MaxProjectCountClient client: maxProjectClients) {
+            System.out.println(client);
+        }
+
+        //-
+        List<MaxSalaryWorker> maxSalaryWorker = queryService.findMaxSalaryWorker();
+        for (MaxSalaryWorker client: maxSalaryWorker) {
+            System.out.println(client);
+        }
+
+        //-
+        List<YoungestEldestWorkers> youngestEldestWorkers = queryService.findYoungestEldestWorkers();
+        for (YoungestEldestWorkers client: youngestEldestWorkers) {
             System.out.println(client);
         }
 
---
+++ src/main/java/SQL_QueryResources/MaxSalaryWorker.java
@@ -0,0 +1,24 @@
+package SQL_QueryResources;
+
+public class MaxSalaryWorker {
+    private String name;
+    private int salary;
+
+    public MaxSalaryWorker(String name, int salary) {
+        this.name = name;
+        this.salary = salary;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getSalary() {
+        return salary;
+    }
+
+    @Override
+    public String toString() {
+        return "Max Salary Worker {Name = " + name + ", salary = " + salary + '}';
+    }
+}
